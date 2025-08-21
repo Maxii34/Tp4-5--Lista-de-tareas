903b1bb9ec33b3e7aca99a7f5c6b95158a4569da
Maxi Ordoñez <exemaxi@outlook.com>
2025-08-21 15:56:00 -0300

    Se agrego los componentes navbar y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import NavForm from './Conponents/nav';
+import Footer from './Conponents/Footer';
 import './App.css'
+
+
 
 function App() {
 
   return (
     <>
-      
+      <main>
+      <NavForm />
+      </main>
+      <Footer />
     </>
   )
 }
